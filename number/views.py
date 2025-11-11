6a621a26d3f84a979131cfd9485acf7ebf6261d2
reharehavycode261 <stagiaire2.vycode@gmail.com>
2025-11-11 12:32:08 +0300

    Implémentation automatique - feature/7a6837cd-697562-1229

--- number/views.py
+++ number/views.py
@@ -1,25 +1,31 @@
-import pickle
-from io import BytesIO
-
-from PIL import Image
-from django.core.exceptions import ValidationError
-from django.http import JsonResponse
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from django.http import HttpResponse
 from django.shortcuts import render
-
-from S4_IA.settings import BASE_DIR
-from number.models import Character
-
+from django.views.decorators.csrf import csrf_exempt
+from io import BytesIO
+import base64
 
 def index(request):
-    if request.method == 'POST':
-        c = Character.objects.create(image=request.FILES.get('image'))
-        print(c.get_prediction())
-        step, res, sep = c.get_solution()
-        return JsonResponse({
-            'pred_recu': ' '.join(c.get_prediction()),
-            'pred_convert': c.get_prediction_str(),
-            'prediction': step,
-            'res_equ': res,
-            'sep_equ': sep
-        })
     return render(request, 'index.html')
+
+@csrf_exempt
+def visualize_equation(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        equation = data['equation']
+        x = np.linspace(-10, 10, 400)
+        try:
+            y = eval(equation)  # Evaluates the equation directly but CAUTION: this is unsafe!
+            fig, ax = plt.subplots()
+            ax.plot(x, y)
+            ax.set(title='Graph of {}'.format(equation), xlabel='x', ylabel='f(x)')
+            buf = BytesIO()
+            plt.savefig(buf, format='png')
+            plt.close(fig)
+            buf.seek(0)
+            return HttpResponse('data:image/png;base64,' + base64.b64encode(buf.getvalue()).decode(), content_type="text")
+        except Exception as e:
+            return HttpResponse(status=400, content=str(e))
+    return HttpResponse(status=405)
